f7565ae2070f2e1130b9d04ee8bcb1c31b651713
johanHdz <joahan_07@hotmail.com>
2023-11-29 16:40:59 -0600

    postgres / crispy_forms / allauth / bootstrap

--- proyectoConfig/settings.py
+++ proyectoConfig/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_#z%mx**1pco%m0#znd-&d*iq*q6w_r3=#b_31_%r$2&r09n7p'
+SECRET_KEY = BASE_DIR / '.env'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
